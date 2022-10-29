0e83db71439cb3e207601674b2420458837c98fa
Jaison Joseph <jaisonj2001@gmail.com>
2022-10-28 21:49:31 -0400

    update README; docs for walkieTalkie

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 ## Dependencies
 
 * [python](https://www.python.org/) 3.8.2 or greater
-* [keyboard](https://pypi.org/project/keyboard/)
 * [pygame](https://www.pygame.org/)
--- WalkieTalkie.py
+++ WalkieTalkie.py
@@ -1,12 +1,32 @@
 from collections import deque
 
+'''
+the walkieTalkie is an abstraction used for the sprites to 'neatly' communicate
+with the Scene
+
+a WalkieTalkie object maintains a queue of messages that any of the 
+sprites as well as the scene can append/read from. 
+
+theoretically the walkieTalkie can be used as a 2 way communication channel 
+between the sprites and the Scene, but for the time being it is used 
+as a 1 way channel just for any of the sprites to signal the scene to stop the main loop
+
+the data structure used is a deque (https://docs.python.org/3/library/collections.html#collections.deque)
+that is optimized for fast append/pop from either sides
+'''
 class WalkieTalkie:
     def __init__(self) -> None:
         self.messages = deque()
 
+    '''
+    add message to queue
+    '''
     def addMessage(self, msg):
         self.messages.append(msg)
 
+    '''
+    returns None if the queue is empty, else returns the earliest message received by the walkieTalkie
+    '''
     def getMessage(self):
         if not self.messages:
             return None
